4b7736d0fff5612c910de86d696a2142dc4815ac
orzAtalod <1759978497@qq.com>
2025-04-29 21:49:23 +0800

    update ArgPath

--- src/stan_command.rs
+++ src/stan_command.rs
@@ -21,7 +21,6 @@ mod arg_error {
     #[derive(Debug)]
     pub enum ArgError {
         NotValidArgTreeType(String),
-        BadPath(String),
         BadArgumentValue(String),
         FileSystemError(std::io::Error),
     }
@@ -30,7 +29,6 @@ mod arg_error {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             match self {
                 Self::NotValidArgTreeType(s) => write!(f, "{s}"),
-                Self::BadPath(s) => write!(f, "{s}"),
                 Self::BadArgumentValue(s) => write!(f, "{s}"),
                 Self::FileSystemError(e) => write!(f, "file system error: {e}"),
             }
@@ -68,7 +66,6 @@ pub trait ArgThrough {
 mod arg_path {
     use std::path::PathBuf;
     use std::fmt::Display;
-    use std::ffi::OsString;
     use super::WithDefaultArg;
     #[derive(Debug, Clone, PartialEq)]
     pub enum ArgPath {
@@ -128,8 +125,101 @@ mod arg_path {
         }
     }
 
-    trait AsFilePath {
-        fn get_path(&self) -> OsString;
+    impl From<&'static str> for ArgPath {
+        fn from(path: &'static str) -> Self {
+            Self::Borrowed(path)
+        }
     }
 
+    impl From<PathBuf> for ArgPath {
+        fn from(path: PathBuf) -> Self {
+            Self::Owned(path)
+        }
+    }
+
+    impl From<ArgPath> for PathBuf {
+        fn from(path: ArgPath) -> Self {
+            match path {
+                ArgPath::Borrowed(path) => PathBuf::from(path),
+                ArgPath::Owned(path) => path,
+            }
+        }
+    }
+
+    impl From<ArgReadablePath> for PathBuf {
+        fn from(path: ArgReadablePath) -> Self {
+            match path {
+                ArgReadablePath::Borrowed(path) => PathBuf::from(path),
+                ArgReadablePath::Owned(path) => path,
+            }
+        }
+    }
+    
+    impl From<ArgWritablePath> for PathBuf {
+        fn from(path: ArgWritablePath) -> Self {
+            match path {
+                ArgWritablePath::Borrowed(path) => PathBuf::from(path),
+                ArgWritablePath::Owned(path) => path,
+            }
+        }
+    }
+
+    use std::io::Error;
+    impl ArgPath {
+        pub fn verify_file_readable(&self) -> Result<(), Error> {
+            match self {
+                ArgPath::Borrowed(path) => std::fs::File::open(path).map(|_|()),
+                ArgPath::Owned(path) => std::fs::File::open(path).map(|_|()),
+            }
+        }
+
+        pub fn into_readable(self) -> Result<ArgReadablePath, Error> {
+            self.verify_file_readable()?;
+            match self {
+                ArgPath::Borrowed(path) => Ok(ArgReadablePath::Borrowed(path)),
+                ArgPath::Owned(path) => Ok(ArgReadablePath::Owned(path)),
+            }
+        }
+
+        pub fn verify_file_writeable(&self) -> Result<(), Error> {
+            let path = match self {
+                ArgPath::Borrowed(path) => PathBuf::from(path),
+                ArgPath::Owned(path) => path.clone(),
+            };
+            if let Some(parent_path) = path.parent() {
+                std::fs::create_dir_all(parent_path)?;
+            }
+            std::fs::OpenOptions::new()
+                    .create(true)
+                    .append(true) // 避免 truncate 覆盖
+                    .open(path)
+                    .map(|_|())
+        }
+
+        pub fn into_writeable(self) -> Result<ArgWritablePath, Error> {
+            self.verify_file_writeable()?;
+            match self {
+                ArgPath::Borrowed(path) => Ok(ArgWritablePath::Borrowed(path)),
+                ArgPath::Owned(path) => Ok(ArgWritablePath::Owned(path)),
+            }
+        }
+
+        pub fn extend_default_file(&mut self, default_name: &str) -> &mut Self {
+            match self {
+                ArgPath::Borrowed(path) => {
+                    let mut path = PathBuf::from(*path);
+                    if path.extension().is_none() {
+                        path.push(default_name);
+                        *self = ArgPath::Owned(path);
+                    }
+                },
+                ArgPath::Owned(path) => {
+                    if path.extension().is_none() {
+                        path.push(default_name);
+                    }
+                },
+            }
+            self
+        }
+    }
 }
